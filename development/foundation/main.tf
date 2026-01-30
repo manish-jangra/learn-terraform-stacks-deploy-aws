52cef237b445212798efe615fc47761345163e54
Manish Kumar <30774250+manish-jangra@users.noreply.github.com>
2026-01-30 17:47:36 +0530

    added

---
+++ development/foundation/main.tf
@@ -0,0 +1,25 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+module "cluster_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name                 = "hcp-development"
+  cidr                 = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  azs             = slice(data.aws_availability_zones.azs.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+}
